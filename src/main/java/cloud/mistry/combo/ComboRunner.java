50a16539e05307e9382cfeda8b690fd12b64ca31
Pritesh <me@pritesh.net>
2020-06-29 13:17:37 +0100

    Added config for AWS CodeGuru

--- src/main/java/cloud/mistry/combo/ComboRunner.java
+++ src/main/java/cloud/mistry/combo/ComboRunner.java
@@ -2,7 +2,7 @@ package cloud.mistry.combo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import software.amazon.codeguruprofilerjavaagent.Profiler;
 
 @SpringBootApplication
 public class ComboRunner
@@ -10,14 +10,14 @@ public class ComboRunner
     public static void main(String[] args)
     {
         // AWS CodeGuru profiler
-//        System.setProperty("aws.region", "eu-west-1");
-//        new Profiler.Builder()
-//                .profilingGroupName("JavaWordsComboProfileGroup")
-//                .build()
-//                .start();
+        System.setProperty("aws.region", "eu-west-1");
+
+        new Profiler.Builder()
+                .profilingGroupName("WordsCombo-ProfilingGroup")
+                .build()
+                .start();
 
         SpringApplication.run(ComboRunner.class, args);
-
 
     }
 }
